4cce03df705c307206922bc0e1259b7ade01f0dc
Irakli <irakli@tchigl.com>
2021-08-12 01:26:34 +0400

    imported and placed components inside app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import About from "./components/About";
+import CodeOfConduct from "./components/CodeOfConduct";
+import Accomodation from "./components/Accomodation";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Some important event</h1>
+      <About />
+      <CodeOfConduct />
+      <Accomodation />
     </div>
   );
 }
