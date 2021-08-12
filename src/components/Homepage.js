545bcf31c684782ffd5dedae0c3b98c0b4d7a821
Irakli <irakli@tchigl.com>
2021-08-12 23:45:36 +0400

    styled button, styling inputs

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,9 +2,17 @@ function Homepage() {
   return (
     <div class="home">
       <h1 id="tagline">Level Up Your React</h1>
-      <input type="text"></input>
-      <input type="select"></input>
-      <button>Buy a Ticket</button>
+      <label class="custom-field">
+        {" "}
+        <span className="placeholder">Enter your name</span>
+        <input type="text"></input>
+      </label>
+      <label class="custom-field">
+        {" "}
+        <span className="placeholder">Select the Date</span>
+        <select></select>
+      </label>
+      <button className="button ticketButton">Buy a Ticket</button>
     </div>
   );
 }
