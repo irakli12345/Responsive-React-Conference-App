ffc0125f45827c0ff9e0965a578c8db666cb5a33
Irakli <irakli@tchigl.com>
2021-08-12 18:42:30 +0400

    styled homepage image

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,7 @@
 function Homepage() {
   return (
     <div>
+      {" "}
       <img
         class="header-img"
         src="https://cdn.pixabay.com/photo/2017/08/10/08/47/laptop-2620118_960_720.jpg"
